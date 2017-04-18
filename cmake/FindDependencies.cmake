eefd492a8ac6faf0b555ed2155c2457b592cd783
Adam Allevato <adam.d.allevato@gmail.com>
2017-04-18 11:49:00 -0700

    Added FindDependencies to look for gflags and ign-common

---
+++ cmake/FindDependencies.cmake
@@ -0,0 +1,9 @@
+include (${PROJECT_CMAKE_DIR}/Utils.cmake)
+
+########################################
+# GFlags
+find_library(gflags_LIBRARIES NAMES gflags)
+find_path(gflags_INCLUDE_DIRS gflags/gflags.h ENV CPATH)
+if (NOT gflags_LIBRARIES OR NOT gflags_INCLUDE_DIRS)
+  BUILD_ERROR("GFlags package is missing.")
+endif()
