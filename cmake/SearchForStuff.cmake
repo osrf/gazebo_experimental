fcbc9b543d6d17d5ad47fd8168ee2ab1cd34e575
Adam Allevato <adam.d.allevato@gmail.com>
2017-04-18 16:45:22 -0700

    Deleted old SearchForStuff.cmake

--- cmake/SearchForStuff.cmake
+++
@@ -1,7 +0,0 @@
-########################################
-# GFlags
-find_library(gflags_LIBRARIES NAMES gflags)
-find_path(gflags_INCLUDE_DIRS gflags/gflags.h ENV CPATH)
-if (NOT gflags_LIBRARIES OR NOT gflags_INCLUDE_DIRS)
-  BUILD_ERROR("GFlags package is missing.")
-endif()
