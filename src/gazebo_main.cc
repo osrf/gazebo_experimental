fabd80f1699c1bfc1ea1294cd8f3a6bd90b280b3
Adam Allevato <adam.d.allevato@gmail.com>
2017-04-19 11:02:01 -0700

    Removed un-implemented command line args from comments

--- src/gazebo_main.cc
+++ src/gazebo_main.cc
@@ -36,36 +36,9 @@ void Help()
 {
   std::cerr << "gazebo -- Run the Gazebo server and GUI.\n" << std::endl;
   std::cerr << "`gazebo` [options] <world_file>\n" << std::endl;
-  // std::cerr << "Gazebo server runs simulation and handles commandline "
-  //   << "options, starts a Master, runs World update and sensor generation "
-  //   << "loops. This also starts the Gazebo GUI client in a separate "
-  //   << "process.\n" << std::endl;
 
   std::cerr << "Options:" << std::endl
-  // << "  -d [ --record ]               Record simulation data." << std::endl
-  // << "  -e [ --physics ] arg          Specify a physics engine "
-  // << "  -f [ --file ] arg             Load an SDF file on start." << std::endl
-  // << "  -g [ --gui ]                  Run only the GUI." << std::endl
   << "  -h [ --help ]                 Print help message." << std::endl
-  // << "  -i [ --iters ] arg            Run simulation for the specified "
-  // <<                                  "number of iterations." << std::endl
-  // <<                                  "(xxx|xxx|xxx)." << std::endl
-  // << "  -l [ --replay ] arg           Replay a log file." << std::endl
-  // << "  --minimal_comms               Reduce the amount of generated TCP/IP"
-  // << "  -o [ --profile ] arg          Physics profile name." << std::endl
-  // << "  -p [ --plugin ] arg           Load a plugin." << std::endl
-  // << "  -r [ --run ]                  Run physics simulation on start."
-  // <<                                  std::endl
-  // << "  --record_encoding arg (=zlib) Compression encoding format for log "
-  // <<                                  "data " << std::endl
-  // << "                                (zlib|bz2|txt)." << std::endl
-  // << "  -s [ --server ]               Run only the server, a.k.a. headless."
-  // <<                                  std::endl
-  // << "  --seed arg                    Start with the given random number seed."
-  // <<                                  std::endl
-  // <<                                  " traffic." << std::endl
-  // << "  -v [ --verbose ] level (=X)   Adjust the level of console output."
-  // <<                                  std::endl
   << "  --version                     Print version information." << std::endl
   << std::endl;
 }
